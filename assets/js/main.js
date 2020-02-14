d4ed92063ae3cdb0c8e79743b22a613543393ff3
Arno van Staden <arnovanstaden@gmail.com>
2020-02-14 12:38:35 +0200

    finish home page

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Animations
+
+$(".contact-info a:nth-child(2)").hover(function() {
+    $(".contact-info a:nth-child(2) i").toggleClass("fa-envelope")
+    $(".contact-info a:nth-child(2) i").toggleClass("fa-envelope-open")
+});
+
+$(".contact-info a:nth-child(3)").hover(function() {
+    $(".contact-info a:nth-child(3) i").toggleClass("fa-map-marker-alt")
+    $(".contact-info a:nth-child(3) i").toggleClass("fa-map-marked-alt")
+})
