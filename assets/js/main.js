c142b3f96a5a94865918bebb69cbe6207215d682
Arno van Staden <arnovanstaden@gmail.com>
2020-02-17 12:20:53 +0200

    update js if pathname

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,30 @@
 // Animations
 
-document.querySelector(".contact-info a:nth-child(2)").addEventListener("mouseover", function () {
-    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope");
-    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope-open");
-});
+if (window.location.pathname == "/index.html" || window.location.pathname == "" || window.location.pathname == "contact.html") {
+    document.querySelector(".contact-info a:nth-child(2)").addEventListener("mouseover", function () {
+        document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope");
+        document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope-open");
+    });
 
-document.querySelector(".contact-info a:nth-child(2)").addEventListener("mouseout", function () {
-    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope");
-    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope-open");
-});
+    document.querySelector(".contact-info a:nth-child(2)").addEventListener("mouseout", function () {
+        document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope");
+        document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope-open");
+    });
 
-document.querySelector(".contact-info a:nth-child(3)").addEventListener("mouseover", function () {
-    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marker-alt")
-    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marked-alt")
-});
+    document.querySelector(".contact-info a:nth-child(3)").addEventListener("mouseover", function () {
+        document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marker-alt")
+        document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marked-alt")
+    });
 
-document.querySelector(".contact-info a:nth-child(3)").addEventListener("mouseout", function () {
-    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marker-alt")
-    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marked-alt")
-});
+    document.querySelector(".contact-info a:nth-child(3)").addEventListener("mouseout", function () {
+        document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marker-alt")
+        document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marked-alt")
+    });
+}
 
 
 // Absolute position navbar home page
 
-if (window.location.pathname == "/index.html" || window.location.pathname == "/index.html") {
+if (window.location.pathname == "/index.html" || window.location.pathname == "") {
     document.querySelector("nav").style.position = "absolute";
 }
