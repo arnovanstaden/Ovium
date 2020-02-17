3ecd5d2d4b417ff6d69b5e567a1f0babc16b1318
Arno van Staden <arnovanstaden@gmail.com>
2020-02-17 10:54:11 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,28 @@
 // Animations
 
-$(".contact-info a:nth-child(2)").hover(function() {
-    $(".contact-info a:nth-child(2) i").toggleClass("fa-envelope")
-    $(".contact-info a:nth-child(2) i").toggleClass("fa-envelope-open")
+document.querySelector(".contact-info a:nth-child(2)").addEventListener("mouseover", function () {
+    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope");
+    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope-open");
 });
 
-$(".contact-info a:nth-child(3)").hover(function() {
-    $(".contact-info a:nth-child(3) i").toggleClass("fa-map-marker-alt")
-    $(".contact-info a:nth-child(3) i").toggleClass("fa-map-marked-alt")
-})
+document.querySelector(".contact-info a:nth-child(2)").addEventListener("mouseout", function () {
+    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope");
+    document.querySelector(".contact-info a:nth-child(2) i").classList.toggle("fa-envelope-open");
+});
+
+document.querySelector(".contact-info a:nth-child(3)").addEventListener("mouseover", function () {
+    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marker-alt")
+    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marked-alt")
+});
+
+document.querySelector(".contact-info a:nth-child(3)").addEventListener("mouseout", function () {
+    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marker-alt")
+    document.querySelector(".contact-info a:nth-child(3) i").classList.toggle("fa-map-marked-alt")
+});
+
+
+// Absolute position navbar home page
+
+if (window.location.pathname == "/index.html" || window.location.pathname == "/index.html") {
+    document.querySelector("nav").style.position = "absolute";
+}
