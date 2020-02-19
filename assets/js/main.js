b2e67db6b5537b970a6ac99f3f84f36c092fbb13
Arno van Staden <arnovanstaden@gmail.com>
2020-02-19 09:43:20 +0200

    update mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,11 +32,76 @@ if (window.location.pathname == "/index.html" || window.location.pathname == "")
 // Mobile Menu
 
 document.querySelector(".mobile-nav-icon").addEventListener("click", function() {
-    document.querySelector(".mobile-nav").style.visibility = "visible";
-    document.querySelector(".mobile-nav").style.opacity = "1";
+    document.querySelector(".mobile-nav").style.width = "80vw";
 });
 
 document.querySelector(".mobile-nav i").addEventListener("click", function() {
-    document.querySelector(".mobile-nav").style.visibility = "hidden";
-    document.querySelector(".mobile-nav").style.opacity = "0";
+    document.querySelector(".mobile-nav").style.width = "0";
+});
+
+
+
+// Scroll Reveal
+
+// Scroll Reveal
+
+window.sr = ScrollReveal({
+    reset: false,
+    mobile: true
+});
+
+sr.reveal('.section-about-info', {
+    delay: 500,
+    duration : 1500,
+});
+
+sr.reveal('.product', {
+    duration : 1500,
+});
+
+sr.reveal('.team-member', {
+    duration : 1500,
+});
+
+// sr.reveal('.section-heading', {
+//     delay: 500,
+//     duration : 2000,
+//     origin: "top",
+//     distance: 200
+// });
+
+sr.reveal('.left-info > div', {
+    delay: 500,
+    duration : 1500,
+    origin: "left",
+    distance: "250px"
+});
+
+sr.reveal('.right-info > div', {
+    duration : 1500,
+    origin: "right",
+    distance: "250px"
+});
+sr.reveal('.page-intro-left > div', {
+    duration : 1500,
+    origin: "left",
+    distance: "250px"
+});
+
+sr.reveal('.page-intro-right > div', {
+    duration : 1500,
+    origin: "right",
+    distance: "250px"
+});
+
+sr.reveal('.about-mission', {
+    duration : 2000,
+    origin: "right",
+    distance: "250px"
+});
+
+sr.reveal('.about-vision', {
+    duration : 2000,
+    origin: "left",
+    distance: "250px"
 });
